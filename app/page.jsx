30ab0d20778f53a6c4291fb7d2a3f6581529b100
Martin O'Shaughnessy <vsredshift@gmail.com>
2025-03-19 14:55:12 +0100

    add pages and components with hard coded data

---
+++ app/page.jsx
@@ -0,0 +1,16 @@
+import Heading from "@/components/Heading";
+import RoomCard from "@/components/RoomCard";
+import rooms from "@/data/rooms.json";
+
+export default function Home() {
+  return (
+    <>
+      <Heading title="Available Rooms" />
+      {rooms.length > 0 ? (
+        rooms.map((room) => <RoomCard room={room} key={room.$id}/>)
+      ) : (
+        <p>No rooms available at the moment</p>
+      )}
+    </>
+  );
+}
